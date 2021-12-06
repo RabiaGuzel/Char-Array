10f4c2c5bdc6328f52dbb12e0636c8b6a2a11362
Rabia Güzel <95624602+RabiaGuzel@users.noreply.github.com>
2021-12-06 20:54:53 +0300

    Random 100 elemanlı karakter dizisi

---
+++ src/Chararray/Chararray.java
@@ -0,0 +1,25 @@
+
+package chararray;
+import java.util.Random;
+public class Chararray {
+    public static char [] diziolustur(){
+        char dizi[]=new char[100];
+        for(int i=0;i<100;i++){
+            Random rnd = new Random();
+            char c = (char) ('a' + rnd.nextInt(26));
+            dizi[i]=c;
+        }
+        return dizi;}
+    public static void diziGoster(char [] dizi){
+        diziolustur();
+        for (int i=0;i<100;i++){
+            System.out.println(" "+dizi[i]);
+       
+        }
+    }
+    public static void main(String[] args) {
+      diziGoster(diziolustur());
+
+    }}
+
+ 
